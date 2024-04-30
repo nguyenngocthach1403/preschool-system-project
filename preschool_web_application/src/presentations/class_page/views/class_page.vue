e11898c3a79b566fd1af0d7a9e7f6d0af459fa88
ThachWynn <146806999+nguyenngocthach1403@users.noreply.github.com>
2024-04-30 11:55:58 +0700

    Thach 30/4 11:55 feat: add create button to open class creation view

--- preschool_web_application/src/presentations/class_page/views/class_page.vue
+++ preschool_web_application/src/presentations/class_page/views/class_page.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="bg-white h-dvh ml-4 rounded-3xl text-center text-3xl  pb-[200px] overflow-hidden">
+        <ClassCreationView class="w-screen h-screen bg-black absolute z-10 left-0 top-0" v-show="showCreateClass" @close="closeCreateClassPage($event)" />
         <!-- Header -->
         <div class="text-left px-[20px] pt-[10px] text-[36px] font-bold ">Class</div>
 
-        <div class="my-3">
+        <div class="my-3 flex justify-between content-center mr-3">
             <SearchFormComp v-on:passSearchText="getSearchText($event)"></SearchFormComp>
+            <CreateButtonComp class="active:scale-95" @click="showCreateClass = !showCreateClass"/>
         </div>
         
         <div class="w-full h-full flex flex-wrap content-start mx-[15px] pb-[20px] overflow-auto">
@@ -18,13 +20,20 @@
 
     import ClassCardComp from '../components//class_card.vue'
     import SearchFormComp from '@/components/search_form_comp.vue'
-
+    import CreateButtonComp from '@/components/create_button.vue'
+    import ClassCreationView from '..//..//class_creation_page//views//class_creation_page.vue'
     const searchText = ref('')
+
+    const showCreateClass = ref(false)
 
     const getSearchText = (event) => {
         searchText.value = event;
     }
     
+    const closeCreateClassPage = (event) =>
+    {
+        showCreateClass.value = event
+    }
 
     const filteredClass = computed(() => {
         return classDataDemo.value.filter((e) => {
