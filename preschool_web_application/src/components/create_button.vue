cca04ac44f68439996252af667939c16584be0e5
ThachWynn <146806999+nguyenngocthach1403@users.noreply.github.com>
2024-05-07 19:42:35 +0700

    Thach 19:42 7/5 style: change layout create button.

--- preschool_web_application/src/components/create_button.vue
+++ preschool_web_application/src/components/create_button.vue
@@ -1,7 +1,7 @@
 <template>
-    <button class="w-fit text-[16px] flex justify-center h-[40px] bg-green-500 rounded-[5px] px-[10px] text-white py-2 shadow-card content-center">
+    <button class="w-fit text-[16px] flex justify-center h-[40px] bg-[#3B44D1] rounded-[5px] px-[10px] text-white py-2 shadow-card content-center hover:bg-blue-600">
         <img :src="create_icon" class="w-[25px]">
-        <div class="ml-2 h-full content-center">Create class</div>
+        <!-- <div class="ml-2 h-full content-center"></div> -->
     </button>
 </template>
 
