40b35206c542eb5c83559b5b49ebe9d5d83cd505
ThachWynn <146806999+nguyenngocthach1403@users.noreply.github.com>
2024-05-22 08:30:43 +0700

    Thach 8:30 22/5 feat: Add Api response image

--- preschool_server/server.js
+++ preschool_server/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const config = require("./src/config/config");
 const morgan = require("morgan");
-
 const app = express();
 const path = require("path");
 
@@ -14,6 +13,8 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "uploads")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
@@ -43,6 +44,7 @@ app.use("/register", require("./src/register/register"));
 app.use("/images", express.static(path.join(__dirname, "uploads")));
 app.use("/account", require("./src/account/account.controller"));
 app.use("/class", require("./src/class/class.controller"));
+app.use("/image", require("./src/image/image.controller"));
 
 app.listen(port, () => {
   console.log(`listen port ${port}`);
